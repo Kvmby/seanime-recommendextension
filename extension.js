38c16658dc52a8afbe14c72aa2e5ce8ba5663aa2
Kvmby <106906688+Kvmby@users.noreply.github.com>
2025-06-28 20:43:48 -0500

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,11 @@
+{
+  "name": "Recommended For You",
+  "id": "recommended-for-you",
+  "version": "1.0.0",
+  "description": "Recommends anime based on your watch history.",
+  "author": "Kvmby",
+  "scripts": ["recommend.js"],
+  "styles": [],
+  "permissions": ["history", "catalog"],
+  "entry": "recommend.js"
+}
