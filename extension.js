2ef840fdb73a1bbd49bd436b0975a6ea59fda966
Kvmby <106906688+Kvmby@users.noreply.github.com>
2025-06-28 20:55:26 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,9 +1,10 @@
 {
-  "name": "Recommended For You",
   "id": "recommended-for-you",
+  "name": "Recommended For You",
   "version": "1.0.0",
   "description": "Recommends anime based on your watch history.",
   "author": "YourName",
+  "language": "javascript",
   "manifest_version": 1,
   "type": "extension",
   "scripts": ["recommend.js"],
